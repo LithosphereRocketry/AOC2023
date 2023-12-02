1998cd28f6a5ebb907d778d835d4608809a84e41
ben <greatmaharg@gmail.com>
2023-12-02 17:31:00 -0500

    Day 2 in SQL woooooo

---
+++ Day2/day2pt2.sql
@@ -0,0 +1,61 @@
+CREATE TEMPORARY TABLE CsvData(
+    row VARCHAR(200)
+);
+
+-- A separator that will never exist so we just get rows
+.separator '~'
+.import input.txt CsvData
+.separator ','
+
+CREATE TEMPORARY TABLE NumbererdRows(
+    id INT PRIMARY KEY,
+    txt VARCHAR(200)
+);
+
+INSERT INTO NumbererdRows
+SELECT CAST(SUBSTRING(row, 5, instr(row, ":")-5) AS INT),
+       REPLACE(SUBSTRING(row, instr(row, ":")+2), ';', ',')
+FROM CsvData WHERE true;
+
+CREATE TEMPORARY TABLE Draws(
+    id INT,
+    color char(1),
+    value INT
+);
+
+WITH RECURSIVE CteRows(id, txt, color, value) AS (
+        SELECT id, txt, ' ', -1
+        FROM NumbererdRows
+
+        UNION ALL
+        
+        SELECT
+            id,
+            IIF(INSTR(txt, ",")=0,
+                NULL,
+                SUBSTRING(txt, INSTR(txt, ",")+2)),
+            SUBSTRING(txt, INSTR(txt, " ")+1, 1),
+            CAST(SUBSTRING(txt, 0, INSTR(txt, " ")) AS INT)
+        FROM CteRows
+        WHERE txt NOT NULL
+    )
+INSERT INTO Draws
+SELECT id, color, value FROM CteRows
+WHERE value >= 0;
+
+CREATE TEMPORARY TABLE Games(
+    id INT PRIMARY KEY,
+    red INT DEFAULT 0,
+    green INT DEFAULT 0,
+    blue INT DEFAULT 0
+);
+
+INSERT INTO Games
+SELECT id, MAX(IIF(color='r', value, 0)),
+           MAX(IIF(color='g', value, 0)),
+           MAX(IIF(color='b', value, 0))
+FROM Draws
+WHERE true
+GROUP BY id;
+
+SELECT SUM(red*green*blue) FROM Games;
